674ba1e12e83cc9191821799db30fbc09f5ea1f5
Brian Nunez <nunez27@Brians-MacBook-Pro.local>
2021-03-22 15:32:24 -0400

    task 3 and 4 completed

--- index.js
+++ index.js
@@ -81,17 +81,27 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(callback, inningsPlayed){
-  /*Code Here*/
+function finalScore(callback, number){
+  let Home = 0;
+  let Away = 0;
+  for (let i = 0; i < number; i++) {
+    Home += callback();
+    Away += callback();
+  }
+  return {Home, Away}
+  // {Home: Home, Away: Away}
 }
+// console.log(finalScore(inning, 5));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(callback) {
+  let homeTeam = callback();
+  let awayTeam = callback();
+  return {Home: homeTeam, Away: awayTeam}
 }
 
 
